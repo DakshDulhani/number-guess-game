441fc648f0d2c6727b049ba182e4a2f3e360b7cc
DakshDulhani <88325548+DakshDulhani@users.noreply.github.com>
2022-08-08 09:23:05 +0530

    Add files via upload

---
+++ numberGame.py
@@ -0,0 +1,9 @@
+Guess a number (between 1 and 9):
+enter your guess :- 3
+your guess was too low: guess a number higher than 3
+Enter your guess :- 4
+your guess was too low: guess a number higher than 4
+enter your guess :- 7
+your guess was too low : guess a number higher than 7
+enter your guess :- 9
+CONGRATULATIONS YOU WON
